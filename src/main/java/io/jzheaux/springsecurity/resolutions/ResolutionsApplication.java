27d9fc8b9abee4d43e5976889e2b8a8ffdfb9def
Josh Cummings <josh.cummings@gmail.com>
2020-05-08 15:59:56 -0600

    Step Six: Database Users
    Now that we've got some users in a database, we can use
    JdbcUserDetailsManager, which uses JDBC to query for users.
    
    Since we haven't added user authorities just yet, we're hard-coding the
    list of authorities for each user to be resolution:read.

--- src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionsApplication.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionsApplication.java
@@ -3,47 +3,29 @@ package io.jzheaux.springsecurity.resolutions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
 import javax.sql.DataSource;
 import java.util.List;
 
-import static org.springframework.http.HttpMethod.GET;
-
 @SpringBootApplication
-public class ResolutionsApplication extends WebSecurityConfigurerAdapter {
+public class ResolutionsApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ResolutionsApplication.class, args);
-    }
+	@Bean
+	public UserDetailsService userDetailsService(DataSource dataSource) {
+		return new JdbcUserDetailsManager(dataSource) {
+			@Override
+			protected List<GrantedAuthority> loadUserAuthorities(String username) {
+				return AuthorityUtils.createAuthorityList("resolution:read");
+			}
+		};
+	}
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests(authz -> authz
-                        .mvcMatchers(GET, "/resolutions", "/resolution/**").hasAuthority("resolution:read")
-                        .anyRequest().hasAuthority("resolution:write"))
-                .httpBasic(basic -> {});
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(ResolutionsApplication.class, args);
+	}
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-        return new InMemoryUserDetailsManager(
-                org.springframework.security.core.userdetails.User
-                        .withUsername("user")
-                        .password("{bcrypt}$2a$10$hSY16mw613UlB3wv8xjaBekZKukxh7e5BcVyd83STJLhQgamjtZu2")
-                        .authorities("resolution:read")
-                        .build());
-    }
-
-    @Bean
-    UserDetailsService userDetailsService(DataSource dataSource) {
-        return new JdbcUserDetailsManager(dataSource);
-    }
 }
