ae4bf07f0286ace3168f2392efbfe32f9556e59d
Josh Cummings <josh.cummings@gmail.com>
2020-05-12 10:24:54 -0600

    Step Eleven: A Minimal UserDetailsService
    A UserDetailsService should throw a UserNotFoundException if it can't
    find a user. For that reason, a simple implementation is one that always
    throws this exception.

--- src/main/java/io/jzheaux/springsecurity/resolutions/UserRepositoryUserDetailsService.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/UserRepositoryUserDetailsService.java
@@ -1,50 +1,12 @@
 package io.jzheaux.springsecurity.resolutions;
 
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class UserRepositoryUserDetailsService implements UserDetailsService {
-    private final UserRepository users;
-
-    public UserRepositoryUserDetailsService(UserRepository users) {
-        this.users = users;
-    }
-
-    private static class BridgeUser extends User implements UserDetails {
-        public BridgeUser(User user) {
-            super(user);
-        }
-
-        public List<GrantedAuthority> getAuthorities() {
-            return this.userAuthorities.stream()
-                    .map(UserAuthority::getAuthority)
-                    .map(SimpleGrantedAuthority::new)
-                    .collect(Collectors.toList());
-        }
-
-        public boolean isAccountNonExpired() {
-            return this.enabled;
-        }
-
-        public boolean isAccountNonLocked() {
-            return this.enabled;
-        }
-
-        public boolean isCredentialsNonExpired() {
-            return this.enabled;
-        }
-    }
-
-    @Override
-    public UserDetails loadUserByUsername(String username) {
-        return this.users.findByUsername(username)
-                .map(BridgeUser::new)
-                .orElseThrow(() -> new UsernameNotFoundException("invalid user"));
-    }
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		throw new UsernameNotFoundException("no user");
+	}
 }
