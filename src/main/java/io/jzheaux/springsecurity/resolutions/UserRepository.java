90908d5669247463c00a785164a03edca2fc8b57
Josh Cummings <josh.cummings@gmail.com>
2020-05-12 10:29:56 -0600

    Step Thirteen: Converting a Custom Domain to UserDetails
    The preferred way to convert from a custom domain object to UserDetails
    is to use a private static class inside your custom UserDetailsService.
    The reason for that is so that your domain object doesn't have any
    dependencies on Spring Security.

--- src/main/java/io/jzheaux/springsecurity/resolutions/UserRepository.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/UserRepository.java
@@ -8,5 +8,5 @@ import java.util.UUID;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, UUID> {
-    Optional<User> findByUsername(String username);
+	Optional<User> findByUsername(String username);
 }
