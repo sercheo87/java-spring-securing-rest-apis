88fe23f764c506ee06d3082268b5ebe3ae6a0758
Josh Cummings <josh.cummings@gmail.com>
2020-05-13 11:59:40 -0600

    Step Fourteen: Accessing domain-specific information
    Your controllers can now gain access to domain-specific information via
    the @CurrentSecurityContext annotation.

--- src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionController.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionController.java
@@ -1,7 +1,11 @@
 package io.jzheaux.springsecurity.resolutions;
 
-import org.springframework.security.core.annotation.CurrentSecurityContext;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.transaction.Transactional;
 import java.util.Optional;
@@ -9,39 +13,39 @@ import java.util.UUID;
 
 @RestController
 public class ResolutionController {
-    private final ResolutionRepository resolutions;
+	private final ResolutionRepository resolutions;
 
-    public ResolutionController(ResolutionRepository resolutions) {
-        this.resolutions = resolutions;
-    }
+	public ResolutionController(ResolutionRepository resolutions) {
+		this.resolutions = resolutions;
+	}
 
-    @GetMapping("/resolutions")
-    public Iterable<Resolution> read() {
-        return this.resolutions.findAll();
-    }
+	@GetMapping("/resolutions")
+	public Iterable<Resolution> read() {
+		return this.resolutions.findAll();
+	}
 
-    @GetMapping("/resolution/{id}")
-    public Optional<Resolution> read(@PathVariable("id") UUID id) {
-        return this.resolutions.findById(id);
-    }
+	@GetMapping("/resolution/{id}")
+	public Optional<Resolution> read(@PathVariable("id") UUID id) {
+		return this.resolutions.findById(id);
+	}
 
-    @PostMapping("/resolution")
-    public Resolution make(@CurrentSecurityContext(expression = "authentication.name") String owner, @RequestBody String text) {
-        Resolution resolution = new Resolution(text, owner);
-        return this.resolutions.save(resolution);
-    }
+	@PostMapping("/resolution")
+	public Resolution make(@CurrentUsername String owner, @RequestBody String text) {
+		Resolution resolution = new Resolution(text, owner);
+		return this.resolutions.save(resolution);
+	}
 
-    @PutMapping(path = "/resolution/{id}/revise")
-    @Transactional
-    public Optional<Resolution> revise(@PathVariable("id") UUID id, @RequestBody String text) {
-        this.resolutions.revise(id, text);
-        return read(id);
-    }
+	@PutMapping(path="/resolution/{id}/revise")
+	@Transactional
+	public Optional<Resolution> revise(@PathVariable("id") UUID id, @RequestBody String text) {
+		this.resolutions.revise(id, text);
+		return read(id);
+	}
 
-    @PutMapping("/resolution/{id}/complete")
-    @Transactional
-    public Optional<Resolution> complete(@PathVariable("id") UUID id) {
-        this.resolutions.complete(id);
-        return read(id);
-    }
+	@PutMapping("/resolution/{id}/complete")
+	@Transactional
+	public Optional<Resolution> complete(@PathVariable("id") UUID id) {
+		this.resolutions.complete(id);
+		return read(id);
+	}
 }
